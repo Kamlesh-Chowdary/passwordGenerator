a39398f566c61d90117eeaac2da9793c901d40a7
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-02-02 13:46:02 -0800

    feat generate password

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useCallback } from "react";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeCharacters, setIncludeCharacters] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let password = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (includeCharacters) str += "~!@#$%^&*(){}[]";
+    if (includeNumbers) str += "1234567890";
+
+    for (let i = 1; i <= length; i++) {
+      let randomIndex = Math.floor(Math.random() * str.length);
+      password += str.charAt(randomIndex);
+    }
+
+    setPassword(password);
+  }, [length, includeNumbers, includeCharacters, setPassword]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="text-4xl text-center text-white py-8">
+        Password Generator
+      </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
